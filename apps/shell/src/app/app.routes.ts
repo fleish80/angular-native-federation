f68f7168a71c86935d77efca9e0d9aa16cf307ce
fleish80 <Fleish.D@gmail.com>
2024-07-14 17:46:22 +0300

    angular native federation

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Route } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/native-federation';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'mfe1',
+    loadComponent: () =>
+      loadRemoteModule('mfe1', './Component').then((m) => m.AppComponent),
+  },
+];
